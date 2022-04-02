3068672f87ce940da9d9dccc9f1f0e5145703c21
Wai-Shing Luk <luk036@gmail.com>
2022-04-02 14:44:19 +0800

    code cleanup

--- tests/test_sp_n.py
+++ tests/test_sp_n.py
@@ -6,7 +6,7 @@ from pylds.discrep_2 import discrep_2
 from pylds.lds_n import CylinN, SphereN
 
 
-def rupylds(spgen):
+def run_pylds(spgen):
     npoints = 600
     Triples = np.array([spgen.pop() for _ in range(npoints)])
     hull = ConvexHull(Triples)
@@ -15,16 +15,16 @@ def rupylds(spgen):
 
 
 def test_sphere_n():
-    spgen = SphereN([2, 3, 5, 7])
-    measure = rupylds(spgen)
+    spgen = SphereN(3)
+    measure = run_pylds(spgen)
     assert measure == approx(0.9125914)
     # assert measure < 0.913
     # assert measure > 0.912
 
 
 def test_cylin_n():
-    cygen = CylinN([2, 3, 5, 7])
-    measure = rupylds(cygen)
+    cygen = CylinN(3)
+    measure = run_pylds(cygen)
     assert measure == approx(1.0505837105828988)
     # assert measure < 1.086
     # assert measure > 1.085
